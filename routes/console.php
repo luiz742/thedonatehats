71d5c7d247575d68aae36b20d558a63666659066
Adrian Fraga <luizfraga742@gmail.com>
2025-04-11 06:32:25 -0300

    continuo tentando

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,11 @@ use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\CheckPendingDonations;
 
+app(Schedule::class)->call(function () {
+    return app(CheckPendingDonations::class)->handle();
+})->everyThirtySeconds();
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
